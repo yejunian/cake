85c86be39483bcbe5fe2c5dde004ba42988318d1
yejunian <leeye51456@gmail.com>
2024-08-06 01:15:22 +0900

    feat: Add and use candle number state

--- src/components/Cake/CandleContainer.tsx
+++ src/components/Cake/CandleContainer.tsx
@@ -1,12 +1,16 @@
+import { useCandleStore } from '@/stores/candle.store';
+
 import Candle from './Candle/Candle';
 import { candles } from './CandleContainer.css';
 
 function CandleContainer() {
+  const { candleNumber } = useCandleStore();
+
   return (
     <div className={candles}>
-      <Candle />
-      <Candle />
-      <Candle />
+      {Array.from({ length: candleNumber }, (_, index) => (
+        <Candle key={index} />
+      ))}
     </div>
   );
 }
--- src/stores/candle.store.ts
+++ src/stores/candle.store.ts
@@ -2,14 +2,20 @@ import { create } from 'zustand';
 
 interface ICandleState {
   isLit: boolean;
+  candleNumber: number;
 }
 
 interface ICandleActions {
   toggleIsLit: () => void;
+  setCandleNumber: (value: number) => void;
 }
 
 export const useCandleStore = create<ICandleState & ICandleActions>((set) => ({
   isLit: true,
+  candleNumber: 1,
 
   toggleIsLit: () => set(({ isLit }) => ({ isLit: !isLit })),
+
+  setCandleNumber: (value) =>
+    set({ candleNumber: Math.max(1, Math.floor(value)) }),
 }));
