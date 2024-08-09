f86f1caaec72db3cc1db27959c6f9e07e9de588a
yejunian <leeye51456@gmail.com>
2024-08-10 03:17:58 +0900

    feat: Replace some flexes with absolute position

--- src/components/Cake/CakeCandles/CakeCandles.css.ts
+++ src/components/Cake/CakeCandles/CakeCandles.css.ts
@@ -1,14 +1,18 @@
 import { style } from '@vanilla-extract/css';
 
 export const candles = style({
+  position: 'absolute',
+  zIndex: '10',
+  right: 0,
+  bottom: '50vmin',
+  left: 0,
+  width: '100%',
+  height: '50vmin',
+
   display: 'flex',
-  justifyContent: 'space-evenly',
+  justifyContent: 'space-evenly', // TODO: 숫자 초 작업 시 초 타입에 따라 space-evenly, center 전환
   alignItems: 'end',
 
   boxSizing: 'border-box',
   padding: '0 10%',
-  width: '100%',
-
-  position: 'relative',
-  zIndex: '10',
 });
--- src/components/Candle/Candle.css.ts
+++ src/components/Candle/Candle.css.ts
@@ -1,10 +1,8 @@
 import { style } from '@vanilla-extract/css';
 
 export const candle = style({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'end',
-  alignItems: 'center',
+  position: 'relative',
 
-  width: '7.5%',
+  width: '7.5%', // TODO: 숫자 초 작업 시 초 타입에 따라 maxHeight와 함께 조정 필요
+  maxHeight: '40vmin',
 });
